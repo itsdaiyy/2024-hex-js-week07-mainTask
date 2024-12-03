039b4162204fe1e90a8b7eb2d1497ae7f4171053
ollie <behoya11@gmail.com>
2024-12-03 14:49:27 +0800

    chore: 修改 vite 設定、gh-pages push repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default {
   base: "/2024-hex-js-week07-mainTask/",
   root: resolve(__dirname, "src"),
   build: {
-    outDir: "../dist",
+    outDir: "./dist",
   },
   server: {
     port: 8080,
